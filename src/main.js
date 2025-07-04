1a050ea64a80d3b2cb1e5084ab2f93fe58d36c81
Kirtan Agravat <kirtan@weybee.in>
2025-07-04 18:20:45 +0530

    part 4 styling Navbar

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
-// import './style.css'
-// import javascriptLogo from './javascript.svg'
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
+const primaryHeader = document.querySelector(".primary-header");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+const primaryNav = document.querySelector(".primary-navigation");
 
-// // document.querySelector('#app').innerHTML = 
+navToggle.addEventListener("click",() => {
+    primaryNav.hasAttribute("data-visible")
+    ? navToggle.setAttribute("aria-expanded",false)
+    :navToggle.setAttribute("aria-expanded",true);
+    primaryNav.toggleAttribute("data-visible")
 
-// setupCounter(document.querySelector('#counter'))
+
+    primaryHeader.toggleAttribute("data-overlay");
+});
